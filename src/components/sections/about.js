8c35908d941f7c43b48bd5d18fe38bff5f16e3cf
Shane Curtis <shane.curtis1027@gmail.com>
2025-02-08 16:52:28 -0500

    update photo

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -162,7 +162,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/me.jpeg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
